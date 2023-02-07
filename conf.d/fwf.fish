0bf08a4d4196ef2681def482796a92146b1b519d
light4 <root@i01.io>
2023-02-08 01:14:51 +0800

    fix insert command

--- conf.d/fwf.fish
+++ conf.d/fwf.fish
@@ -4,3 +4,7 @@
 if not status is-interactive && test "$CI" != true
     exit
 end
+
+function set_current_cmd --on-event fish_preexec
+    set -xU CURRENT_CMD $argv
+end
--- functions/fwf.fish
+++ functions/fwf.fish
@@ -2,7 +2,7 @@ function fwf -d "fwf - Filter With Feedback"
     # set current_file $(status --current-filename)
 
     __fwf_main $argv | read result
-    set removed_fwf "$(echo -E $history[1] | sed -E 's/(.*)fwf /\1/g')"
+    set removed_fwf "$(echo -E $CURRENT_CMD | sed -E 's/(.*)fwf /\1/g')"
     set new_cmd "$removed_fwf $result"
     commandline -i $new_cmd
 end
