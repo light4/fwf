8c301997b5a616c3b1108df355b738403bc227e4
light4 <root@i01.io>
2023-02-07 23:19:59 +0800

    fix use in interactive mode

--- conf.d/fwf.fish
+++ conf.d/fwf.fish
@@ -1,5 +1,10 @@
 #!/usr/bin/env fish
 
+# fwf.fish is only meant to be used in interactive mode.
+if not status is-interactive && test "$CI" != true
+    exit
+end
+
 # set current_file $(status --current-filename)
 
 set result "$(__fwf_main /dev/stdin $argv)"
