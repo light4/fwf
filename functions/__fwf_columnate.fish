5be874a3aedf4f7d20e7bb725a79e0544c0e483d
light4 <root@i01.io>
2023-02-07 23:57:40 +0800

    fix wrap

--- functions/__fwf_columnate.fish
+++ functions/__fwf_columnate.fish
@@ -1,11 +1,12 @@
 function __fwf_columnate -d "fwf columnate"
     function wrap
-        set cols $(tput cols)
-        set halfcols $(math round $cols/2)
+        set -l cols $(tput cols)
+        set -l halfcols $(math round $cols/2)
         fold -s -w $halfcols "$argv[1]";
     end
 
     function columnate
+        set -l cols $(tput cols)
         pr -w $cols -m -t (wrap "$argv[1]" | psub) (wrap "$argv[2]" | psub)
     end
 
